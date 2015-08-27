cf6c4ecf483b6442d084cec345e68654ea4140f5
Pravjot Rai <pravjot.s.rai@gmail.com>
2015-08-27 14:19:36 -0700

    character panel
    added main character panel to bottom left corner

---
+++ Project_Falcon/Assets/Scripts/HUD/_CharacterPanelManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _CharacterPanelManager : MonoBehaviour {
+
+    private enum enPanelState
+    {
+        collapsed,
+        expanded
+    };
+
+    private enPanelState characterPanelState;
+
+	// Use this for initialization
+	void Start () {
+        characterPanelState = enPanelState.collapsed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changePanelState(){
+
+        if(characterPanelState == enPanelState.collapsed)
+        {
+            print("collapsed -> expanded");
+            characterPanelState = enPanelState.expanded;
+        }
+        else if (characterPanelState == enPanelState.expanded)
+        {
+            print("expanded -> collapsed");
+            characterPanelState = enPanelState.collapsed;
+        }
+    }
+}
