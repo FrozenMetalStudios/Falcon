a2045d8ca428213ae762417a4b2f5229636eccd7
Pravjot Rai <pravjot.s.rai@gmail.com>
2015-08-31 14:57:41 -0700

    Code clean up
    merged the TabManager and CharacterPanelManager files into two files.
    The _HUDManager file contains all the onclick functions used to provide
    functionality for all HUD user interface events. The _MacroViewManager
    contains the MacroUI namespace which contains all the classes associated
    with the MacroView UI.
    -Generic Management Panel class
    -Small Management Panel class
    -Large Management Panel class
    -Character Panel class
    These contain all initialization and supporting functions for each panel
    class

---
+++ Project_Strategy/Assets/Scripts/HUD/MacroViewUI.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MacroViewUI
+{
+
+    public class ManagementPanel
+    {
+        private GameObject armyTab;
+        private GameObject captainsTab;
+        private GameObject controlledAreasTab;
+        private GameObject resourcesTab;
+        private GameObject technologyTab;
+        private GameObject optionsTab;
+
+        public ManagementPanel()
+        {
+            armyTab = GameObject.FindGameObjectWithTag("Army_small");
+            captainsTab = GameObject.FindGameObjectWithTag("Captains_small");
+            controlledAreasTab = GameObject.FindGameObjectWithTag("ControlledAreas_small");
+            resourcesTab = GameObject.FindGameObjectWithTag("Resources_small");
+            technologyTab = GameObject.FindGameObjectWithTag("Technology_small");
+            optionsTab = GameObject.FindGameObjectWithTag("Options_small");
+        }
+
+        public GameObject ArmyTab
+        {
+            get { return armyTab; }
+            set { armyTab = value; }
+        }
+        public GameObject CaptainsTab
+        {
+            get { return captainsTab; }
+            set { captainsTab = value; }
+        }
+        public GameObject ControlledAreasTab
+        {
+            get { return controlledAreasTab; }
+            set { controlledAreasTab = value; }
+        }
+        public GameObject ResourcesTab
+        {
+            get { return resourcesTab; }
+            set { resourcesTab = value; }
+        }
+        public GameObject TechnologyTab
+        {
+            get { return technologyTab; }
+            set { technologyTab = value; }
+        }
+        public GameObject OptionsTab
+        {
+            get { return optionsTab; }
+            set { optionsTab = value; }
+        }
+
+        public void contentHide()
+        {
+            armyTab.SetActive(false);
+            captainsTab.SetActive(false);
+            controlledAreasTab.SetActive(false);
+            resourcesTab.SetActive(false);
+            technologyTab.SetActive(false);
+            optionsTab.SetActive(false);
+        }
+        public GameObject findTabObject(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    return armyTab;
+                case 2:
+                    return captainsTab;
+                case 3:
+                    return controlledAreasTab;
+                case 4:
+                    return resourcesTab;
+                case 5:
+                    return technologyTab;
+                case 6:
+                    return optionsTab;
+                default:
+                    return null;
+            }
+        }    
+
+    }
+
+    public class SmallManagementPanel : ManagementPanel
+    {
+        public SmallManagementPanel()
+        {
+            base.ArmyTab = GameObject.FindGameObjectWithTag("Army_small");
+            base.CaptainsTab = GameObject.FindGameObjectWithTag("Captains_small");
+            base.ControlledAreasTab = GameObject.FindGameObjectWithTag("ControlledAreas_small");
+            base.ResourcesTab = GameObject.FindGameObjectWithTag("Resources_small");
+            base.TechnologyTab = GameObject.FindGameObjectWithTag("Technology_small");
+            base.OptionsTab = GameObject.FindGameObjectWithTag("Options_small");
+        }
+    }
+
+    public class LargeManagementPanel : ManagementPanel
+    {
+        private GameObject maximizedPanel;
+
+        public LargeManagementPanel()
+        {
+            maximizedPanel = GameObject.FindGameObjectWithTag("maximizedManagementPanel");
+            base.ArmyTab = GameObject.FindGameObjectWithTag("Army_large");
+            base.CaptainsTab = GameObject.FindGameObjectWithTag("Captains_large");
+            base.ControlledAreasTab = GameObject.FindGameObjectWithTag("ControlledAreas_large");
+            base.ResourcesTab = GameObject.FindGameObjectWithTag("Resources_large");
+            base.TechnologyTab = GameObject.FindGameObjectWithTag("Technology_large");
+        }
+
+        public GameObject MaximizedPanel
+        {
+            get { return maximizedPanel; }
+            set { maximizedPanel = value; }
+        }
+
+        public void contentHide()
+        {
+            ArmyTab.SetActive(false);
+            CaptainsTab.SetActive(false);
+            ControlledAreasTab.SetActive(false);
+            ResourcesTab.SetActive(false);
+            TechnologyTab.SetActive(false);
+        }
+
+    }
+
+    public class CharacterPanel
+    {
+        //enum for collaspe and expanded state of the character window
+        public enum enPanelState
+        {
+            collapsed,
+            expanded
+        };
+
+        private enPanelState currentPanelState;
+        private GameObject collapsedPanel;
+        private GameObject expandedPanel;
+
+        public GameObject CollapsedPanel
+        {
+            get { return collapsedPanel; }
+            set { collapsedPanel = value; }
+        }
+
+        public GameObject ExpandedPanel
+        {
+            get { return expandedPanel; }
+            set { expandedPanel = value; }
+        }
+
+        public enPanelState CurrentPanelState
+        {
+            get { return currentPanelState; }
+            set { currentPanelState = value; }
+        }
+
+        public CharacterPanel()
+        {
+            //get the game objects of the collapsed and expanded panels
+            collapsedPanel = GameObject.FindGameObjectWithTag("collapsed");
+            expandedPanel = GameObject.FindGameObjectWithTag("expanded");
+
+        }
+
+    }
+}
---
+++ Project_Strategy/Assets/Scripts/HUD/_MacroViewManager.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+using MacroViewUI;
+
+public class _MacroViewManager : MonoBehaviour {
+
+    private static double CLOCK_TIC = 0.2;
+    private enum enMouseClick
+    {
+        SingleClick,
+        DoubleClick,
+        None
+    }
+
+    private GameObject maximizedPanel;
+    private float lastClick = 0;
+    private SmallManagementPanel smallMP;
+    private LargeManagementPanel largeMP;
+    private CharacterPanel characterPanel;
+
+    void Start()
+    {
+        smallMP = new SmallManagementPanel();
+        largeMP = new LargeManagementPanel();
+        characterPanel = new CharacterPanel();
+
+        smallMP.contentHide();
+        smallMP.ArmyTab.SetActive(true);
+        largeMP.contentHide();
+        largeMP.MaximizedPanel.SetActive(false);
+        characterPanel.CollapsedPanel.SetActive(true);
+        characterPanel.ExpandedPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("m"))
+        {
+            if(largeMP.MaximizedPanel != null)
+            {
+                largeMP.contentHide();
+                if (largeMP.MaximizedPanel.activeSelf == true)
+                {
+                    largeMP.MaximizedPanel.SetActive(false);
+                }
+                else {
+                    largeMP.MaximizedPanel.SetActive(true);
+                    largeMP.ArmyTab.SetActive(true);
+                }
+            }
+        }
+    }
+
+    /*************************************************************************************************
+     *                                          Onclick Functions
+     *************************************************************************************************/
+    public void smallPanelTabChange(int id)
+    {
+        smallMP.contentHide();
+        enMouseClick mouseClick = tabClick();
+
+        if (largeMP.MaximizedPanel != null)
+        {
+            if (mouseClick == enMouseClick.DoubleClick && largeMP.MaximizedPanel.activeSelf == false)
+            {
+                //open big window
+                largeMP.MaximizedPanel.SetActive(true);
+                largeMP.contentHide();
+                findTabObject(id, largeMP).SetActive(true);
+            }
+            else if (mouseClick == enMouseClick.DoubleClick && largeMP.MaximizedPanel.activeSelf == true)
+            {
+                //close big window
+                largeMP.MaximizedPanel.SetActive(false);
+            }
+        }
+
+        findTabObject(id, smallMP).SetActive(true);
+    }
+    public void largePanelTabChange(int id)
+    {
+        largeMP.contentHide();
+        enMouseClick mouseClick = tabClick();
+
+        if (largeMP.MaximizedPanel != null)
+        {
+            if (mouseClick == enMouseClick.DoubleClick && largeMP.MaximizedPanel.activeSelf == false)
+            {
+                //open big window
+                largeMP.MaximizedPanel.SetActive(true);
+            }
+            else if (mouseClick == enMouseClick.DoubleClick && largeMP.MaximizedPanel.activeSelf == true)
+            {
+                //close big window
+                largeMP.MaximizedPanel.SetActive(false);
+            }
+        }
+
+        findTabObject(id, largeMP).SetActive(true);
+    }
+
+    public void changePanelState()
+    {
+
+        if (characterPanel.CurrentPanelState == MacroViewUI.CharacterPanel.enPanelState.collapsed)
+        {
+            characterPanel.CurrentPanelState = MacroViewUI.CharacterPanel.enPanelState.expanded;
+            characterPanel.CollapsedPanel.SetActive(false);
+            characterPanel.ExpandedPanel.SetActive(true);
+        }
+        else if (characterPanel.CurrentPanelState == MacroViewUI.CharacterPanel.enPanelState.expanded)
+        {
+            characterPanel.CurrentPanelState = MacroViewUI.CharacterPanel.enPanelState.collapsed;
+            characterPanel.ExpandedPanel.SetActive(false);
+            characterPanel.CollapsedPanel.SetActive(true);
+        }
+    }
+
+    private GameObject findTabObject(int id, ManagementPanel panel)
+    {
+        switch (id)
+        {
+            case 1:
+                return panel.ArmyTab;
+            case 2:
+                return panel.CaptainsTab;
+            case 3:
+                return panel.ControlledAreasTab;
+            case 4:
+                return panel.ResourcesTab;
+            case 5:
+                return panel.TechnologyTab;
+            case 6:
+                return panel.OptionsTab;
+            default:
+                return null;
+        }
+    }
+    private enMouseClick tabClick()
+    {
+
+        enMouseClick mouseClick;
+        if (Time.time - lastClick < CLOCK_TIC)
+        {
+            mouseClick = enMouseClick.DoubleClick;
+        }
+        else
+        {
+            mouseClick = enMouseClick.SingleClick;
+        }
+        lastClick = Time.time;
+        return mouseClick;
+    }
+}
